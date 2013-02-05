ccfd39d70527ea18982aba39da39c3f9e4417f28
Yu Lei <yuleiwhu@gmail.com>
2013-02-05 14:07:57 +0100

    change main_diverse_multi_voxel_simu

--- main_diverse_multi_voxel_simu.m
+++ main_diverse_multi_voxel_simu.m
@@ -24,36 +24,47 @@ close all
 % The simulated data will be the two gaussian centered pulsive
 % set two centers
 step = 0.05;
-mu1 = 1.5; mu2 = 1;
-daat = (1.2-0.7):step:(2.2-step);
+mu1 = 1.5; mu2 = 1;mu3 = 1.7;
+daat = (1.2-0.7):step:(2.2-0.1);
 rho = 0.8;
-eta1 = pdf('norm',daat,mu1,0.05)*rho;
-eta2 = pdf('norm',daat,mu2,0.05)*(1-rho);
-eta = (eta1+eta2)/sum(eta1+eta2);
+eta1 = pdf('norm',daat,mu1,0.01);
+eta2 = pdf('norm',daat,mu2,0.01);
+eta3 = pdf('norm',daat,mu3,0.01);
 
 
 T = 1.2:0.1:2.2;
 asl = cPASL();
 ccbf = 100;
 M0 = 800;
-perfusion = asl.paslModel([eta(:),daat(:)],'buxton',T)*M0*ccbf;
 
-figure(110),plot(daat,eta);
-figure(111),plot(T,perfusion);
 
 % Data preparing
 w = 5;h=5;
-dataorg = zeros(h,w,length(perfusion));
-for i = 1:length(perfusion)
-    dataorg(:,:,i) = perfusion(i);
+H = 2*h;W=2*w;
+dataorg = zeros(H,W,length(length(T)));
+% patch 1 data
+perfusion1 = asl.paslModel([eta1(:),daat(:)],'buxton',T)*M0*ccbf;
+perfusion2 = asl.paslModel([eta2(:),daat(:)],'buxton',T)*M0*ccbf;
+perfusion3 = asl.paslModel([eta3(:),daat(:)],'buxton',T)*M0*ccbf;
+perfusion4 = asl.paslModel([eta1(:)+eta2(:),daat(:)],'buxton',T)*M0*ccbf;
+
+
+for i = 1:length(T)
+    dataorg(1:h,1:w,i) = perfusion1(i);
+    dataorg(h+1:end,1:w,i) = perfusion2(i);
+    dataorg(h+1:end,w+1:end,i) = perfusion3(i);
+    dataorg(1:h,w+1:end,i) = perfusion4(i);
 end
-data = dataorg + randn(h,w,length(perfusion))*1.5; 
+data = dataorg + randn(H,W,length(T))*0; 
+I = cMRI(data);I.showMRI
+
+% return;
 fprintf('Measurement SNR: %2f\n',snr(dataorg(:),data(:)))
 data(data<0) = 0;
 fprintf('Measurement SNR after zero threshold: %2f\n',snr(dataorg(:),data(:)))
 
 % Dictionary
-DicAAT = (1.2-0.7):step:(2.2-step);
+DicAAT = (1.2-0.7):step:(2.2-0.1);
 N = length(DicAAT); % dictionary width
 M = length(T); % dictionary height
 Phi = zeros(M,N);
@@ -69,8 +80,9 @@ end
 %% Show results
 spect = cMRI(spectrum);
 spect.showMRI;
-cbf = cMRI(sum(spectrum(:,:,10:end-10),3));
+cbf = cMRI(sum(spectrum(:,:,1:end),3));
 cbf.showMRI;
+
 %%
 % [p] = [22,45];
 [p] = [2,2];
@@ -92,13 +104,13 @@ xlabel('AAT');
 ylabel('Distribution of AAT');
 
 %%
-est_snr = 0;
-for i = 1:w
-    for j= 1:h
-        etaest = squeeze(spectrum(i,j,:));
-        etaest = etaest/sum(etaest);
-        est_snr = est_snr + snr(eta,etaest);
-    end
-end
-disp(est_snr/w/h);
+% est_snr = 0;
+% for i = 1:w
+%     for j= 1:h
+%         etaest = squeeze(spectrum(i,j,:));
+%         etaest = etaest/sum(etaest);
+%         est_snr = est_snr + snr(eta,etaest);
+%     end
+% end
+% disp(est_snr/w/h);
 
